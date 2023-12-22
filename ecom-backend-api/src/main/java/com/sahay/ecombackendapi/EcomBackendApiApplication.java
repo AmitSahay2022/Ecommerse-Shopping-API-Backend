74733c72df8d05a6872474a626f76124fa06acec
Sahay Software <sahay.mtech2013@gmail.com>
2023-12-22 14:21:01 +0530

    Project created

---
+++ ecom-backend-api/src/main/java/com/sahay/ecombackendapi/EcomBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.sahay.ecombackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomBackendApiApplication.class, args);
+	}
+
+}
---
+++ ecom-backend-api/src/test/java/com/sahay/ecombackendapi/EcomBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahay.ecombackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
